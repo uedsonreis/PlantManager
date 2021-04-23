3e928307ca442643d684d81c000aaf4bf3418e7f
Uedson Reis <uedsonreis@MacReis.local>
2021-04-23 12:44:40 -0300

    Day four (partial).

--- src/pages/PlantSave/index.tsx
+++ src/pages/PlantSave/index.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import { SvgFromUri } from 'react-native-svg';
 import { useRoute } from '@react-navigation/core';
-import { Alert, Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { Alert, Image, Platform, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import DateTimePicker, { Event } from '@react-native-community/datetimepicker';
 
 import styles from './styles';
 import { Plant } from '../../models';
 import Button from '../../components/Button';
 import waterDrop from '../../assets/waterdrop.png';
+import { isBefore } from 'date-fns';
 
 export default function PlantSave() {
 
     const route = useRoute();
     const { plant } = route.params as { plant: Plant };
+
+    const [ selectedDateTime, setSelectedDateTime ] = React.useState(new Date());
+    const [ showDatePicker, setShowDatePicker ] = React.useState(Platform.OS === 'ios');
+
+    function handleChangeTime(event: Event, date: Date | undefined) {
+        if (Platform.OS === 'android') {
+            setShowDatePicker(old => !old);
+        }
+
+        if (date) {
+            if (isBefore(date, new Date())) {
+                setSelectedDateTime(new Date());
+                Alert.alert('Escolha uma hora no futuro! ⏱');
+            } else {
+                setSelectedDateTime(date);
+            }
+        }
+    }
 
     return (
         <View style={styles.container}>
@@ -23,7 +43,7 @@ export default function PlantSave() {
                     {plant.name}
                 </Text>
                 <Text style={styles.plantAbout}>
-                    Escreva algo sobre a planta aqui.
+                    {plant.about}
                 </Text>
             </View>
 
@@ -32,13 +52,20 @@ export default function PlantSave() {
                 <View style={styles.tipContainer}>
                     <Image style={styles.tipImage} source={waterDrop} />
                     <Text style={styles.tipText}>
-                        Texto qualquer aqui.
+                        {plant.waterTips}
                     </Text>
                 </View>
 
                 <Text style={styles.alertLabel}>
                     Escolha o melhor horário para ser lembrado:
                 </Text>
+
+                <DateTimePicker
+                    value={selectedDateTime}
+                    mode="time"
+                    display='spinner'
+                    onChange={handleChangeTime}
+                />
 
                 <Button text="Cadastrar Planta" />
 
